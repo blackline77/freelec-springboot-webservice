52e77b32abd2e2bbdda59ddc4b3281569d28bbd2
HongKyung Kim <blackline91@naver.com>
2022-03-12 23:42:54 +0900

    커밋

--- src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/HelloResponseDto.java
@@ -1,2 +1,12 @@
-package com.jojoldu.book.springboot.web.dto;public class HelloResponseDTO {
+package com.jojoldu.book.springboot.web.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class HelloResponseDto {
+    private final String name;
+    private final int amount;
+
 }
